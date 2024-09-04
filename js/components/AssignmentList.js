61e5fcdac5ae57fd0778982a4bcbd607c3294ced
Ammar <rajaammar557@gmail.xom>
2024-09-04 18:22:56 +0500

    Ep8: Learn form submission

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -6,9 +6,9 @@ export default {
     },
 
     template: `
-        <section v-show="assignments.length">
+        <section class='mb-4' v-show="assignments.length">
         <h2 class="font-bold mb-2">{{ title }}</h2>
-        <ul>
+        <ul class="border border-gray-600 divide-y divide-gray-600 rounded-md">
         <assignment v-for="assignment in assignments" :key="assignment.id" :assignment="assignment"></assignment>
         </ul>
     </section>
--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -5,27 +5,27 @@ export default {
     template: `
     <assignment-list :assignments="filters.inPrograss" title="In Progress"></assignment-list>
     <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
+
+    <form @submit.prevent="add">
+        <div class="border border-gray-600 rounded-md mt-5 text-black">
+            <input v-model="newAssignment" placeholder="New Assignment..." class="p-2" />
+            <button class="bg-blue-800 py-2 px-4 border-l text-gray-200">Add</button>
+        </div>
+    </form>
     `,
 
     data() {
         return {
             assignments: [
-                {
-                    id: 1,
-                    name: "Finish project",
-                    complete: true,
-                },
-                {
-                    id: 2,
-                    name: "Read chapter 3",
-                    complete: false,
-                },
-                {
-                    id: 3,
-                    name: "Complete their homework",
-                    complete: false,
-                },
+                { id: 1, name: "Finish project", complete: true, },
+                { id: 2, name: "Read chapter 3", complete: false, },
+                { id: 3, name: "Do their homework", complete: false, },
+                { id: 4, name: "Test Yourslef", complete: false, },
+                { id: 5, name: "Read a book", complete: false, },
+                { id: 6, name: "Complete their homework", complete: false, },
             ],
+
+            newAssignment: '',
         }
     },
 
@@ -37,5 +37,17 @@ export default {
                 completed: this.assignments.filter(assignments => assignments.complete),
             }
         }
-    }
+    },
+    methods: {
+        add(e) {
+            e.preventDefault();
+            this.assignments.push({
+                id: this.newAssignment.length + 1,
+                name: this.newAssignment,
+                complete: false,
+            }),
+
+                this.newAssignment = ''
+        }
+    },
 }
