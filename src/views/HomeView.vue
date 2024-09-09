fa29cd95e044d12cadf8d9d2e44e51de131e99da
Ammar <rajaammar557@gmail.xom>
2024-09-09 18:23:45 +0500

    Ep18, Learne basics of Composition API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+
+// eslint-disable-next-line no-undef
+let message = $ref("Welcome There");
+
+setTimeout(() => {
+  message = 'CHANGE';
+}, 3000);
+
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <p>{{ message }}</p>
+    <input type="text" name="" id="" v-model="message">
   </main>
 </template>
--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import ReactivityTransform from '@vue-macros/reactivity-transform/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    ReactivityTransform(),
   ],
   resolve: {
     alias: {
