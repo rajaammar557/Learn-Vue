5d528e4be84cd13d1d7413868fe1800aff1cd647
Ammar <rajaammar557@gmail.xom>
2024-09-10 18:16:39 +0500

    Ep 19, Learn Composables

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,13 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-</template>
+<script setup>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+import { useFlash } from '@/composables/useFlash.js';
+
+let { flash } = useFlash();
+
+</script>
+
+<template>
+  <p>
+    <button @click="flash('Its Work on About page')">Click me</button>
+  </p>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,15 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 
-// eslint-disable-next-line no-undef
-let message = $ref("Welcome There");
+import { useFlash } from '@/composables/useFlash.js';
 
-setTimeout(() => {
-  message = 'CHANGE';
-}, 3000);
+let { flash } = useFlash();
 
 </script>
 
 <template>
   <main>
-    <TheWelcome />
-    <p>{{ message }}</p>
-    <input type="text" name="" id="" v-model="message">
+    <p>
+      <button @click="flash('Its Work')">Click me</button>
+    </p>
   </main>
 </template>
