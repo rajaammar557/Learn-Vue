062807880ee44fd2964066cfffb3aebab19d4f7d
Ammar <rajaammar557@gmail.com>
2024-09-17 18:00:35 +0500

    Ep29, Learn Transition

---
+++ src/components/Model.vue
@@ -0,0 +1,40 @@
+<script setup>
+defineProps({
+    show: Boolean
+})
+</script>
+
+<template>
+    <transition 
+    enter-from-class="opacity-0 scale-125"
+    enter-to-class="opacity-100"
+    enter-active-class="transition duration-400"
+    leave-active-class="transition duration-300"
+    leave-from-class="opacity-100"
+    leave-to-class="opacity-0 scale-125">
+
+        <div v-if="show" class="fixed inset-0 bg-black/70 flex items-center justify-center">
+            <div class="bg-white text-black dark:bg-gray-800 dark:text-white w-9/12 max-w-[500px] p-2 rounded">
+                <div>
+                    <header>
+                        <slot name="header">Default header</slot>
+                    </header>
+                </div>
+
+                <div>
+                    <slot>Default Body</slot>
+                </div>
+
+                <div>
+                    <footer class="mt-3 border-t pt-1 dark:border-gray-600">
+                        <slot name="footer">
+                            <button @click="$emit('close')"
+                                class="text-sm px-3 py-1 mt-1 rounded bg-gray-300 hover:bg-gray-400 dark:bg-gray-600 dark:hover:bg-gray-700 transition-all">Close</button>
+                        </slot>
+                    </footer>
+                </div>
+            </div>
+        </div>
+    </transition>
+
+</template>
